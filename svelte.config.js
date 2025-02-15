b90369658da85b872e0d0f72b41b09f5027a8583
Paul jessey <ophantomo990@gmail.com>
2025-02-15 14:04:41 +0100

    'saved'

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,20 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+    vite: {
+      build: {
+        rollupOptions: {
+          external: ['js-cookie', 'axios'] // 👈 Add this line
+        }
+      }
+    }
+  },
+  preprocess: vitePreprocess()
 };
+
+export default config;
