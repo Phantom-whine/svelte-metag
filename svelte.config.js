9df521154c23f348d70575b7a8fed02d75de29ba
Paul jessey <ophantomo990@gmail.com>
2025-02-15 19:16:29 +0100

    'saved-wrangler'

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter(),
+        vite: {
+            build: {
+                rollupOptions: {
+                    external: ['js-cookie', 'axios'],
+                },
+            },
+        },
+    },
 };
 
 export default config;
